69fdd43f174a2ac30318a8f45d2cca332eafae54
Cherrylee Martinez <actuary.cherrylee.martinez@gmail.com>
2013-10-24 02:01:59 -0400

    Sources a series of Java script commands

---
+++ JavaScript_branch1.js
@@ -0,0 +1,38 @@
+"vocabulary".length*12; 
+//gives you the length of the word in quotations then multiplies that number 12
+
+"Please create a new paragraph.".length;
+//gives the length of a string, including its spaces and punctuation
+
+confirm("Are you sure you want to exit this screen?");
+//Pops an alert questioning the user if they are sure they want to exit.  
+
+prompt("For security purposes, please provide your pin:");
+//prompts a box to open up asking for the user's pin and box for the user to insert their pin.  
+
+"Tomorrow is Wednesday".length<2;
+//provides a false statement as it is a booleen with a length greater than 2
+
+if ("yellow".length*12<20)
+	{
+	alert("The length of yellow multiplied by 12 is less than 20");
+	}
+else
+	{
+	console.log("false");
+	}
+// if returns to advise that statement is false
+
+
+if ("phone".length==4)
+	{
+	console.log("I am wrong");
+	}
+else("phone".length>=4)
+	{
+	console.log("I am right");
+	}
+//processes if statement and finds length of phone is greater than 4 and therefore return the statement "I am right" 
+
+alert("This concludes tonight's session");
+//alerts a pop up with the statement in quotations
